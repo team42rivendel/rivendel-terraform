b0b7feeb20505f1bf05336f43035a6e71513b67e
team42rivendel <team42@rivendel.com.br>
2018-12-06 11:41:39 -0200

    remove sg

--- main.tf
+++ main.tf
@@ -5,24 +5,3 @@ provider "aws" {
   }
 }
 
-
-resource "aws_security_group" "allow_all" {
-  name        = "teste-atlantis-terraform"
-  description = "Allow all inbound traffic"
-  vpc_id      = "vpc-a7aaa3de"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-}
-
