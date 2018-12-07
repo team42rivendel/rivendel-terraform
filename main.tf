4a8af40fc599bfe1568a0d2da57b207634c73973
team42rivendel <team42@rivendel.com.br>
2018-12-07 07:39:07 -0200

    fix region

--- main.tf
+++ main.tf
@@ -10,8 +10,6 @@ resource "aws_security_group" "allow_all" {
   name        = "allow_all_atlantis"
   description = "Allow all inbound traffic"
   vpc_id      = "vpc-a7aaa3de"
-  region      = "us-east-1"
-
   ingress {
     from_port   = 0
     to_port     = 0
